4231ed8c6b79ee259cb6e9533c5dc1e9d891dee2
Thidar Aung <thidar28430146@gmail.com>
2026-07-23 18:13:26 +0700

    contact section

---
+++ my-portfolio/src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import { projects } from '../data/project';
+import ProjectCard from './ProjectCard';
+
+function Contact() {
+    return (
+        <section id="contact" className="border-t border-line px-4 py-4 sm:px-8 sm:py-8">
+            <h1>
+                This contact section
+            </h1>
+        </section>
+    );
+}
+
+export default Contact;
