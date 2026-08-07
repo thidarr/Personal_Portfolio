622309a40950649467d11f3e07431ff9fdf8c909
Thidar Aung <thidar28430146@gmail.com>
2026-08-07 15:39:49 +0700

    project section updated

--- my-portfolio/src/components/Project.jsx
+++ my-portfolio/src/components/Project.jsx
@@ -6,7 +6,7 @@ function Projects() {
         <section id="projects" className="px-4 md:px-8 ">
             <div className='border-t border-b border-line md:py-8 py-7'>
             <p className="font-mono text-accent mb-3  md:text-left text-center ">01. What I've built</p>
-            <h2 className="text-4xl sm:text-5xl font-display text-white mb-5 md:text-left text-center">Projects</h2>
+            <h2 className="text-4xl md:text-5xl font-display text-white mb-5 md:text-left text-center">Projects</h2>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 {projects.map((project) => (
--- my-portfolio/src/components/ProjectCard.jsx
+++ my-portfolio/src/components/ProjectCard.jsx
@@ -1,20 +1,22 @@
 function ProjectCard({ project }) {
     return (
-        <div className="border border-border rounded-lg p-6 hover:-translate-y-1 transition-transform duration-200">
-            <h3 className="text-xl font-medium text-white mb-2">{project.title}</h3>
-            <p className="text-text mb-4">{project.description}</p>
-            <div className="flex flex-wrap gap-2 mb-4">
+        <div className="border border-border rounded-lg md:text-left text-center p-6 hover:-translate-y-1 transition-transform duration-200">
+            <h3 className="md:text-xl text-lg font-medium text-white mb-4">{project.title}</h3>
+            <p className="text-text text-sm mb-5 leading-relaxed ">{project.description}</p>
+            <div className="flex flex-wrap gap-2 mb-5 md:justify-start justify-center">
                 {project.stack.map((tech) => (
                     <span key={tech} className="font-mono text-xs text-accent border border-accent rounded-full px-2 py-1">
                         {tech}
                     </span>
                 ))}
             </div>
-            <div className="flex gap-4">
-                <a href={project.demoLink} className="font-mono text-sm text-text-h hover:text-accent hover:underline">
-                    {project.demolabel} →
-                </a>
-                <a href={project.githubLink} className="font-mono text-sm text-text-h hover:text-accent hover:underline">
+            <div className="flex gap-4 md:justify-start justify-center">
+                {project.demoLink && (
+                    <a href={project.demoLink} target="_blank" rel="noopener noreferrer" className="font-mono text-sm text-text-h hover:text-accent hover:underline">
+                        {project.demoLabel} →
+                    </a>
+                )}
+                <a href={project.githubLink} target="_blank" rel="nopener noreferrer" className="font-mono text-sm text-text-h hover:text-accent hover:underline">
                     GitHub →
                 </a>
             </div>
