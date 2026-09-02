9f33e5cdd8a0df71eddef365719b08dcfdaee757
Thidar Aung <thidar28430146@gmail.com>
2026-09-02 14:57:55 +0700

    project updated

--- my-portfolio/src/data/project.js
+++ my-portfolio/src/data/project.js
@@ -2,8 +2,7 @@ export const projects = [
     {
         id: 1,
         title: "Segmenting and Predicting Tourist Spending Behavior For Optimized Marketing",
-        description: "Analyzed 5,000+ tourist records using Python to uncover spending patterns and traveler segments. Built a Random Forest model to predict spending levels, with findings presented at ICTHM 2025.",
-        stack: ["Python", "pandas", "Random Forest", "Matplotlib"],
+        description: "Analyzed tourist spending behavior by integrating and preprocessing two tourism datasets using Python. Developed and tuned a Random Forest classifier to segment tourists into high-, medium-, and low-spending groups, achieving 75.57% cross-validation accuracy, with the research presented at ICTHM 2025.", stack: ["Python", "pandas", "Random Forest", "Matplotlib"],
         demoLink: "/tourist_spending_analysis.pdf",
         demoLabel: "View Report",
         githubLink: "https://github.com/thidarr/tourist_spending_analysis",
@@ -12,7 +11,7 @@ export const projects = [
         id: 2,
         title: "Agent-Based Modeling of Student Productivity & Mental Health",
         description: "Simulated student productivity and mental health using agent-based modeling with a Cognitive-Behavioral Resource Framework. High-performance habits raised long-term productivity from 0.3 to 0.8 over 800 simulated steps, validated with linear regression (R² = 0.4608).",
-        stack: ["Python", "Agent-Based Modeling", "Linear Regression" ,"Matplotlib"],
+        stack: ["Python", "Agent-Based Modeling", "Linear Regression", "Matplotlib"],
         demoLink: "/Data_Analytics_in_Agent_Based_Modeling.pdf",
         demoLabel: "View Report",
         githubLink: "https://github.com/thidarr/Agent-Based-Modeling-of-Student-Productivity-and-Mental-Health",
